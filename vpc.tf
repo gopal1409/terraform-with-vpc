b6d092c3d1ebc943ece6912949aea7fe0cd5d8f9
John cena <cena@gmail.com>
2021-01-17 22:53:51 -0700

    ec2 instance with custom vpc and public ip

--- vpc.tf
+++ vpc.tf
@@ -14,6 +14,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "app-public-1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
   availability_zone = "us-east-2a"
   tags = {
     Name = "app-public-1"
