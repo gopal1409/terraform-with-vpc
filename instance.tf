9a3153a07fe62cdb40e744c3036f9c59ce0eed83
John cena <cena@gmail.com>
2021-01-17 22:02:40 -0700

    ec2 in custom vpc

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "example" {
 
     # the vpc subnet
     subnet_id = aws_subnet.app-public-1.id 
-    vpc_security_groups_ids = [aws_security_group.allow-ssh]
+    vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 
     #the public ssh key_name
     key_name = aws_key_pair.mykeypair.key_name
--- vars.tf
+++ vars.tf
@@ -10,8 +10,10 @@ variable "PATH_TO_PUBLIC_KEY" {
 
 variable "AMIS"{
     type = map(string)
+    default = {
     us-east-1 = "ami-0be2609ba883822ec "
     us-east-2 = "ami-0a0ad6b70e61be944"
     us-west-1 = "ami-03130878b60947df3"
     us-west-2 = "ami-0a36eb8fadc976275"
+    }
 }
