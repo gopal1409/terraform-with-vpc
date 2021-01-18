ff020ca1f2b722c3f90544a4034ab051364ff7f2
John cena <cena@gmail.com>
2021-01-17 21:56:58 -0700

    custom vpc with ec2 instance

---
+++ instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "example" {
+    ami = var.AMIS[var.AWS_REGION]
+    instance_type = "t2.micro"
+
+    # the vpc subnet
+    subnet_id = aws_subnet.app-public-1.id 
+    vpc_security_groups_ids = [aws_security_group.allow-ssh]
+
+    #the public ssh key_name
+    key_name = aws_key_pair.mykeypair.key_name
+}
--- vars.tf
+++ vars.tf
@@ -1,3 +1,17 @@
 variable "AWS_REGION"{
     default = "us-east-2"
 }
+variable "PATH_TO_PRIVATE_KEY" {
+    default = "gopalkey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+    default = "gopalkey.pub"
+}
+
+variable "AMIS"{
+    type = map(string)
+    us-east-1 = "ami-0be2609ba883822ec "
+    us-east-2 = "ami-0a0ad6b70e61be944"
+    us-west-1 = "ami-03130878b60947df3"
+    us-west-2 = "ami-0a36eb8fadc976275"
+}
